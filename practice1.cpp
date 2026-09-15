0ec614ecdd4c07e07d1a188565758b3abc33af7e
yumo <2186949908@qq.com>
2026-09-15 21:23:09 +0800

    完成第五题：猜数字游戏
    - 首次使用 while 循环 + break
    - 用 srand/rand 生成 1-100 随机数
    - 输入失败保护（if (!std::cin) break）
    - 统计并输出猜测次数

--- practice1.cpp
+++ practice1.cpp
@@ -1,28 +1,32 @@
-﻿// practice1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>     // 1. 引入输入输出库
-
-int main()              // 2. 程序入口
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+int main(void)
 {
-	for (int i = 1; i <= 9; i++)
-	{        // 外层：控制"行"
-		for (int j = 1; j <= i; j++)
-		{			// 内层：控制"这一行里的第几个"
-			std::cout << j << "*" << i << "=" << i * j<<"\t"; // 打印一个算式
+	srand(time(0));
+	int secret = rand() % 100 + 1;
+	int guess;
+	int count = 0;
+	while (true)
+	{
+		std::cin >> guess;
+		if (!std::cin)break;
+		count++;
+		if (guess == secret)
+		{
+			std::cout << "猜对了" << "\n";
+			break;
 		}
-		std::cout << "\n";                 // 一行打完了，换行！
+		else if (guess < secret)
+		{
+			std::cout << "太小" << "\n";
+		}
+		else
+		{
+			std::cout << "太大" << "\n";
+		}
+		std::cout << std::endl;
 	}
-	return 0;           //    告诉系统"正常结束"
+	std::cout << "共猜了" << count << "次" << std::endl;
+	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
