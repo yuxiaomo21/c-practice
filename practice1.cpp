65bd00f9d7a39a0e322da7edbad7395aed90560e
yumo <2186949908@qq.com>
2026-09-16 20:29:17 +0800

    完成第八题：数组工具函数
    - 拆分为 calcSum / findMax / reverseArray 三个函数
    - main 变成清晰的调用目录
    - 数组参数传地址，reverseArray 原地修改生效
    - 两处编译器警告（C4700/C4715）已修正

--- practice1.cpp
+++ practice1.cpp
@@ -1,26 +1,57 @@
 ﻿#include <iostream>
-int main()
+
+// 函数定义要写在 main 上面（否则 main 里"看不到"它们）
+
+int calcSum(int arr[], int n)
+{
+	int a=0;
+	for (int i = 0; i < n; i++)
 	{
+		a += arr[i];
+	}
+	return a;
+	// 累加求和，return 结果
+}
+
+int findMax(int arr[], int n)
+{
+	int max = arr[0];
+	for (int i = 1; i < n; i++)
+	{
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
+	}
+	return max;
+	// 找最大值，return 结果
+	// 提示：还记得第六题的套路吗？
+}
+
+void reverseArray(int arr[], int n)
+{
+	for (int i = 0, j = n - 1; i < j; i++, j--)//我理解的是i为最小值，j为里面的最大值，i和j相互交换，直到i=j为止
+	{
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+		// 交换 arr[i] 和 arr[j]
+		// 提示：还记得第七题的交换三步骤吗？
+	}
+}
+
+int main()
+{
 	int n;
-	int a, b;
 	std::cin >> n;
 	int arr[100];
-	for (int i = 0; i < n; i++)
-	{
-		std::cin >> arr[i];
-	}
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n -1; j++)
-		{
-			if (arr[j] < arr[j + 1])
-			{
-				int temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
-			}
-		}
-		std::cout << arr[n-1-i] << " ";
-	}
-		return 0;
-	}
+	for (int i = 0; i < n; i++) std::cin >> arr[i];
+
+	std::cout << calcSum(arr, n) << "\n";
+	std::cout << findMax(arr, n) << "\n";
+
+	reverseArray(arr, n);
+	for (int i = 0; i < n; i++) std::cout << arr[i] << " ";
+	std::cout << "\n";
+	return 0;
+}
