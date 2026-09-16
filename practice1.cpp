46d7dc13174dff8799260c7e1eee19049411c8a7
yumo <2186949908@qq.com>
2026-09-16 21:30:52 +0800

    完成第九题：字符串字符统计
    - 首次使用 std::string 和 getline（读取含空格的整行）
    - 用 isalpha/isdigit/isspace 分类统计
    - 区分 cin >> 与 getline 的差异
    - 修正输出位置（移出统计循环）

--- practice1.cpp
+++ practice1.cpp
@@ -1,57 +1,36 @@
-﻿#include <iostream>
-
-// 函数定义要写在 main 上面（否则 main 里"看不到"它们）
-
-int calcSum(int arr[], int n)
-{
-	int a=0;
-	for (int i = 0; i < n; i++)
-	{
-		a += arr[i];
-	}
-	return a;
-	// 累加求和，return 结果
-}
-
-int findMax(int arr[], int n)
-{
-	int max = arr[0];
-	for (int i = 1; i < n; i++)
-	{
-		if (arr[i] > max)
-		{
-			max = arr[i];
-		}
-	}
-	return max;
-	// 找最大值，return 结果
-	// 提示：还记得第六题的套路吗？
-}
-
-void reverseArray(int arr[], int n)
-{
-	for (int i = 0, j = n - 1; i < j; i++, j--)//我理解的是i为最小值，j为里面的最大值，i和j相互交换，直到i=j为止
-	{
-		int temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
-		// 交换 arr[i] 和 arr[j]
-		// 提示：还记得第七题的交换三步骤吗？
-	}
-}
-
+﻿#include<iostream>
+#include<string>
+#include<cctype>
 int main()
 {
-	int n;
-	std::cin >> n;
-	int arr[100];
-	for (int i = 0; i < n; i++) std::cin >> arr[i];
-
-	std::cout << calcSum(arr, n) << "\n";
-	std::cout << findMax(arr, n) << "\n";
-
-	reverseArray(arr, n);
-	for (int i = 0; i < n; i++) std::cout << arr[i] << " ";
-	std::cout << "\n";
+	std::string s;//初始化字符串
+	std::getline(std::cin, s);
+	int total = (int)s.length();//获取字符串长度
+	int alpha = 0, digit = 0, space = 0, other = 0;
+	for (int i = 0; i < (int)s.length(); i++)
+	{
+		char c = s[i];
+		if (std::isalpha(c))
+		{
+			alpha++;
+		}
+		else if (std::isdigit(c))
+		{
+			digit++;
+		}
+		else if (std::isspace(c))
+		{
+			space++;
+		}
+		else
+		{
+			other++;
+		}
+	}
+	std::cout << total << "\n";
+	std::cout << alpha << "\n";
+	std::cout << digit << "\n";
+	std::cout << space << "\n";
+	std::cout << other << "\n";
 	return 0;
 }
